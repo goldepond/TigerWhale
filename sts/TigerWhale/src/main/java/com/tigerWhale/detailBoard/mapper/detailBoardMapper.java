c5dd334af73a0f899300b86d65912abf41bf6f6a
goldepond <goldepond@naver.com>
2021-07-12 01:22:51 +0900

    20210712

---
+++ sts/TigerWhale/src/main/java/com/tigerWhale/detailBoard/mapper/detailBoardMapper.java
@@ -0,0 +1,9 @@
+package com.tigerWhale.detailBoard.mapper;
+
+import com.tigerWhale.command.UsersVO;
+
+public interface detailBoardMapper {
+	
+	public UsersVO getUserDetail (String id); //상세
+	
+}
---
+++ sts/TigerWhale/src/main/java/com/tigerWhale/detailBoard/service/detailBoardService.java
@@ -0,0 +1,12 @@
+package com.tigerWhale.detailBoard.service;
+
+import java.util.ArrayList;
+
+import com.tigerWhale.command.UsersVO;
+
+
+public interface detailBoardService {
+	public UsersVO getUserDetail(String id); //상세
+
+}
+

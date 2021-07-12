a68c35360289f55a6bb33d57f404346fa50058e0
goldepond <goldepond@naver.com>
2021-07-12 16:11:11 +0900

    20210712

--- sts/TigerWhale/src/main/java/com/tigerWhale/detailBoard/service/detailBoardService.java
+++ sts/TigerWhale/src/main/java/com/tigerWhale/detailBoard/service/detailBoardService.java
@@ -2,11 +2,24 @@ package com.tigerWhale.detailBoard.service;
 
 import java.util.ArrayList;
 
+import com.tigerWhale.command.D_T_boardVO;
+import com.tigerWhale.command.MainBoardVO;
 import com.tigerWhale.command.UsersVO;
+import com.tigerWhale.command.Y_M_boardVO;
+import com.tigerWhale.command.catagoryBoardVO;
+import com.tigerWhale.command.detailBoardVO;
+import com.tigerWhale.command.repyBoardVO;
 
 
 public interface detailBoardService {
-	public UsersVO getUserDetail(String id); //상세
+	public catagoryBoardVO getcatagory(int bno);
+	public ArrayList<D_T_boardVO> getD_T_board(int bno);
+	public ArrayList<detailBoardVO> getBoardDetail(int bno);
+	public MainBoardVO getMainDetail(int bno);
+	public ArrayList<repyBoardVO> getrepyBoard(int bno);
+	public UsersVO getUserDetail(String id);
+	public ArrayList<Y_M_boardVO> getY_M_board(int bno);
+	
 
 }
 
--- sts/TigerWhale/src/main/java/com/tigerWhale/detailBoard/service/detailBoardServiceImpl.java
+++ sts/TigerWhale/src/main/java/com/tigerWhale/detailBoard/service/detailBoardServiceImpl.java
@@ -1,23 +1,62 @@
 package com.tigerWhale.detailBoard.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tigerWhale.command.D_T_boardVO;
+import com.tigerWhale.command.MainBoardVO;
 import com.tigerWhale.command.UsersVO;
+import com.tigerWhale.command.Y_M_boardVO;
+import com.tigerWhale.command.catagoryBoardVO;
+import com.tigerWhale.command.detailBoardVO;
+import com.tigerWhale.command.repyBoardVO;
 import com.tigerWhale.detailBoard.mapper.detailBoardMapper;
 
-@Service("detailBoardService") //componentscan
+@Service("detailBoardService") // componentscan
 public class detailBoardServiceImpl implements detailBoardService {
 
 	@Autowired
 	private detailBoardMapper detailBoardMapper;
-	
+
+	@Override
+	public catagoryBoardVO getcatagory(int bno) {
+		// TODO Auto-generated method stub
+		return detailBoardMapper.getcatagory(bno);
+	}
+
+	@Override
+	public ArrayList<D_T_boardVO> getD_T_board(int bno) {
+		return detailBoardMapper.getD_T_board(bno);
+	}
+
+	@Override
+	public ArrayList<detailBoardVO> getBoardDetail(int bno) {
+		// TODO Auto-generated method stub
+		return detailBoardMapper.getBoardDetail(bno);
+	}
+
+	@Override
+	public MainBoardVO getMainDetail(int bno) {
+		// TODO Auto-generated method stub
+		return detailBoardMapper.getMainDetail(bno);
+	}
+
+	@Override
+	public ArrayList<repyBoardVO> getrepyBoard(int bno) {
+		return detailBoardMapper.getrepyBoard(bno);
+	}
+
 	@Override
 	public UsersVO getUserDetail(String id) {
+		// TODO Auto-generated method stub
 		return detailBoardMapper.getUserDetail(id);
 	}
 
-	
-
+	@Override
+	public ArrayList<Y_M_boardVO> getY_M_board(int bno) {
+		return detailBoardMapper.getY_M_board(bno);
+	}
 
 }

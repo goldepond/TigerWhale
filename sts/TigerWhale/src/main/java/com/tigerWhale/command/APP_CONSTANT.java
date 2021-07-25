dd6289a6f3c2ebda06eece1d0bba869f7a6c536f
MF1204 <kstydevil@gmail.com>
2021-07-25 13:20:35 +0900

    GGU_20210725_1320

--- sts/TigerWhale/src/main/java/com/tigerWhale/command/APP_CONSTANT.java
+++
@@ -1,5 +0,0 @@
-package com.tigerWhale.command;
-
-public class APP_CONSTANT {
-	public static final String UPLOAD_PATH = "A:\\Creative\\Project\\TeamProject\\spring-tool-suite-3.9.14.RELEASE-e4.16.0-win32-x86_64\\sts-bundle\\Workspace\\TigerWhale\\sts\\TigerWhale\\src\\main\\webapp\\resources\\img";
-}

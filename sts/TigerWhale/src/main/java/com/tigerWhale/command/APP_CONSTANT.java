2ce1653471f4d2db9f371818909699bdc1e57c9a
seulee0862 <suelee0862@gmail.com>
2021-07-25 13:40:47 +0900

    LSJ 0725 0140

--- sts/TigerWhale/src/main/java/com/tigerWhale/command/APP_CONSTANT.java
+++
@@ -1,5 +0,0 @@
-package com.tigerWhale.command;
-
-public class APP_CONSTANT {
-	public static final String UPLOAD_PATH = "C:\\project\\TigerWhale\\sts\\TigerWhale\\src\\main\\webapp\\resources\\img";
-}
--- sts/TigerWhale/src/main/java/com/tigerWhale/controller/APP_CONSTANT.java
+++
@@ -1,7 +0,0 @@
-package com.tigerWhale.controller;
-
-//경로나 어플리케이션 전역에서 사용되는 속성을 상수로 지정
-public class APP_CONSTANT {
-	
-	public static final String UPLOAD_PATH = "C:\\project\\TigerWhale\\sts\\TigerWhale\\src\\main\\webapp\\resources\\img";
-}
